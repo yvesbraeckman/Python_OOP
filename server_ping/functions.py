3a589a6285e15481f33ebbd77be921d086fc99d1
Yves Braeckman <yves.braeckman.06@gmail.com>
2023-04-07 14:40:01 +0200

    added html page

--- server_ping/functions.py
+++ server_ping/functions.py
@@ -12,10 +12,15 @@ def main():
         fileObject = open("sites.json", "r")
         jsonContent = fileObject.read()
         sites = json.loads(jsonContent)
+        ping_results = []
+        site_list = []
         while opperating_mode != 0:
             if opperating_mode == 1:  # pings sites in list
                 for diction in sites:
                     print(myping(diction["adress"]))
+                    ping_results.append(myping(diction["adress"]))
+                    site_list.append(diction["adress"])
+                print(ping_results)
             elif opperating_mode == 2:  # shows sites in list
                 print(sites)
             elif opperating_mode == 3:  # add or delete site
@@ -39,6 +44,34 @@ def main():
         jsonFile = open("sites.json", "w")
         jsonFile.write(jsonString)
         jsonFile.close()
+
+        # HTML maken
+
+        html = open("index.html", "w")
+
+        # reset html to an empty one
+        template = open("template.html", "r")
+        interface = template.read()
+        template.close()
+
+        liList = []
+        for i in range(len(ping_results)):
+            if ping_results[i]:
+                testRes = "Connection success"
+                color = "[green]"
+            else:
+                testRes = "Connection failed"
+                color = "[red]"
+
+            liList.append(f"\n\t\t\t<p>{site_list[i]}</p>\n\t\t\t<p class=\"{ping_results[i]}\">{testRes}</p>")
+            # amount of tabs based on length of address
+            tab = int(3 - float(len(site_list[i]) / 15)) * "\t"
+            print(f"{site_list[i]}{tab}{color}{testRes}")
+        print("\n")
+        liString = "\n\t\t</li>\n\t\t<li>".join(liList)
+        interface = interface.replace('''<ul></ul>''', f"<ul>\n\t\t<li>{liString}\n\t\t</li>\n\t</ul>")
+        html.write(f"{interface}")
+        html.close()
     return None
 
 
